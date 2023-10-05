750a69f62cee68c21ecda5023d2bff38986247fb
kanadiasalsa <141605399+kanadiasalsa@users.noreply.github.com>
2023-10-05 08:53:36 +0700

    Add files via upload

---
+++ kanadia salsabila a_XI-9_LUAS SEGITIGA.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main() {
+    double luas;
+    int alas;
+    int tinggi;
+    cout << "Masukkan Alas :";
+    cin >> tinggi;
+    cout << "Masukkan Tinggi :";
+    cin >> alas;
+    luas = alas * tinggi * 0.5;
+    cout << luas << endl;
+    return 0;
+}
+
+;;
